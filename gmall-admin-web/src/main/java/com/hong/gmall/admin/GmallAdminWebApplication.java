058ea71c0c8bca3bc62b0a3ceead952b882c8c8b
hong <485865188@qq.com>
2020-02-16 12:15:47 +0800

    项目结构创建完成

---
+++ gmall-admin-web/src/main/java/com/hong/gmall/admin/GmallAdminWebApplication.java
@@ -0,0 +1,16 @@
+package com.hong.gmall.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hong
+ */
+@SpringBootApplication
+public class GmallAdminWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallAdminWebApplication.class, args);
+    }
+
+}
