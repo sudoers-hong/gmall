2c285ed91fa79162762af70efac675ba0d60da09
hong <485865188@qq.com>
2020-03-17 21:26:49 +0800

    ELK

--- gmall-pms/src/test/java/com/hong/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/hong/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,22 @@
 package com.hong.gmall.pms;
 
+import com.hong.gmall.pms.entity.Product;
+import com.hong.gmall.pms.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GmallPmsApplicationTests {
 
+    @Autowired
+    ProductService productService;
+
     @Test
     void contextLoads() {
+        Product byId = productService.getById(1);
+        String name = byId.getName();
+        System.out.println(name);
     }
 
 }
